2e25e8310ce8424863556bdebedbc592fe27a9a2
Carlos-CF <carloscaetanofernandes@hotmail.com>
2024-09-06 10:36:10 -0300

    Inicializando o Projeto

---
+++ src/main/java/com/br/Sac/SacApplication.java
@@ -0,0 +1,13 @@
+package com.br.Sac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/Sac/SacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Sac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
